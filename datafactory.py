Fixed calculation of observable

datafactory.py
@@ -7,15 +7,17 @@ Created on Wed Jan  5 16:27:00 2022
 
 import torch
 from torch.utils.data import Dataset
+from numpy import pi
 
 class DataFactory(Dataset):
-    def __init__(self, batch_size: int, input_dim: int):
+    def __init__(self, batch_size: int, n_qubits: int, input_dim: int):
         self.batch_size = batch_size 
+        self.n_qubits = n_qubits
         self.input_dim = input_dim
-    
-    def next_batch(self):
-        X = torch.rand((self.batch_size, self.input_dim,1,1,1)).cdouble()*2 -1
-        Y = torch.rand((self.batch_size, self.input_dim,1,1,1)).cdouble()*2 -1
+            
+    def next_batch(self):        
+        X = torch.rand((self.batch_size, self.input_dim,1,1,1))*2*pi
+        Y = torch.rand((self.batch_size,1,1,1)).cdouble()*2 -1
         return X, Y
     
     

layers.py
@@ -29,7 +29,7 @@ class Rx_layer(nn.Module):
             self.weights.cdouble()
         else:
             if weights.shape[1] == 1 and self.num_qubits > 1:
-                self.weights = weights.repeat(1, self.num_qubits)
+                self.weights = weights.repeat(1, self.num_qubits, 1,1,1)
             elif weights.shape[0] == batch_size and weights.shape[1] == len(qubits):
                 self.weights = weights
             else:
@@ -41,7 +41,7 @@ class Rx_layer(nn.Module):
         b = (self.weights/2).sin()
         identity = torch.eye(2)
         off_identity = torch.Tensor([[0,1],[1,0]])
-        return a*identity - 1j*b*off_identity
+        return a*identity - b*off_identity
         
            
     def forward(self, state):
@@ -73,7 +73,7 @@ class Rz_layer(nn.Module):
             nn.init.uniform_(self.weights, 0, 2*np.pi)
         else:
             if weights.shape[1] == 1 and len(qubits) > 1:
-                self.weights = weights.repeat(1, len(qubits),1,1,1)
+                self.weights = weights.repeat(1, self.num_qubits,1,1,1)
             elif weights.shape[0] == batch_size and weights.shape[1] == len(qubits):
                 self.weights = weights
             else:
@@ -116,19 +116,18 @@ class Entangle_layer(nn.Module):
         state = state.repeat(1,1,2,1,1)
         reps = state.shape[2] // 2
         U = self.U.repeat_interleave(reps, dim=2)
-        print(state.shape)
-        print(U.shape)
+        num_repeat_indices = 1
         for i,j in self.qubit_pairs:
-            if i in indices or j in indices:
-                state = state.repeat(1,1,2,1,1)
-                U = self.U.repeat_interleave(2, dim=2)
-                print(U.shape)
-                print(state.shape)
-
-            state[:,i] = torch.matmul(U, state[:,i])
-            state[:,j] = torch.matmul(U, state[:,j])
             indices.append(i)
             indices.append(j)
+            unique, count = np.unique(indices, return_counts=True)
+            if count.max() > num_repeat_indices:
+                state = state.repeat(1,1,2,1,1)
+                U = U.repeat_interleave(2, dim=2)
+                num_repeat_indices += 1
+                    
+            state[:,i] = torch.matmul(U, state[:,i])
+            state[:,j] = torch.matmul(U, state[:,j])
             
         return state
   

main.py
@@ -15,7 +15,7 @@ Created on Mon Dec 27 10:10:55 2021
 
 
 
-from model import BasicModel
+from model import BasicModel, TestModel
 import torch.nn as nn
 import torch
 import matplotlib.pyplot as plt
@@ -29,9 +29,9 @@ def train(model, optimizer, batch_size, n_qubits):
     optim=optimizer
     criterion = nn.MSELoss()
 
-    dataset = DataFactory(batch_size, n_qubits)
+    dataset = DataFactory(batch_size, n_qubits, 10)
     losses = []
-    for epoch in range(4):
+    for epoch in range(300):
         x, y = dataset.next_batch() 
         
         state, output = model(x)
@@ -45,40 +45,34 @@ def train(model, optimizer, batch_size, n_qubits):
         losses.append(loss.item())
     return losses
 
+times = []
+QUBITS = [*range(5, 13)]
+for q in QUBITS:
+    start = time.time()
+    n_qubits = q
+    batch_size = 1
+    model = BasicModel(n_qubits, batch_size)
+    optim = torch.optim.AdamW(model.parameters(), lr=0.05)
+    L = train(model, optim, batch_size, n_qubits)
+    end = time.time()
+    times.append(end-start)
+    
+plt.plot(QUBITS, times)
+plt.xlabel("Qubits")
+plt.ylabel("Time, s")
+plt.title("")
+# plt.savefig('DCdimTiming')
+    
+# n_qubits = 2
+# batch_size = 1
+# model = TestModel(n_qubits, batch_size,1)
+# s, O = model(0)
 
-start = time.time()
-n_qubits = 3
-batch_size = 1
-model = BasicModel(n_qubits, batch_size)
+
 # psi, O = model("")
-# P = torch.zeros(8,1)
+# P = torch.zeros(16,1)
 # psi1 = psi[0]
 # for i in range(psi1.shape[1]):
-#     p = torch.kron(psi1[0,i], torch.kron(psi1[1,i], psi1[2,i]))
+#     p = krons(psi1[:,i])
 #     P = torch.add(P, p)
 # print(P)
-    
-optim = torch.optim.AdamW(model.parameters(), lr=0.05)
-# L = train(model, optim, batch_size, n_qubits)
-# end = time.time()
-# print(end-start)
-# losses = []
-# for i in range(10):
-#     train_features, train_labels = next(iter(train_dataloader))
-#     print(train_features, train_labels)
-#     state, O = model(train_features)
-#     print(state.shape)
-#     loss = criterion(O.real, train_labels)
-        
-        
-#     #Backpropagation
-#     optim.zero_grad()
-#     loss.backward(retain_graph=True)
-#     optim.step()
-        
-#     losses.append(loss.item())
-
-
-# plt.plot(losses, alpha=0.5)
-#plt.hlines(1, 0, 500, colors="r")
-#plt.ylim(0.9, 1.1)

model.py
@@ -10,17 +10,16 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-class BasicModel(nn.Module):
-    def __init__(self, n_qubits: int, batch_size: int):
+class TestModel(nn.Module):
+    def __init__(self, n_qubits: int, batch_size: int, DC: int):
         super().__init__()   
         self.n_qubits = n_qubits
         self.batch_size = batch_size
-        self.fR1 = self.fRot([*range(n_qubits)], batch_size=batch_size) 
-        self.fR2 = self.fRot([*range(n_qubits)], batch_size=batch_size) 
-        Z = torch.Tensor([[1,0],[0,-1]]).cdouble().reshape((1,1,1,2,2))
-        self.Observable = Z.repeat((batch_size, n_qubits, 1, 1, 1))
-        self.Entangle_1 = Entangle_layer([(0,1), (1,2)])
-        self.Entangle_2 = Entangle_layer([(1,2)])
+        Z = torch.Tensor([[1,0],[0,-1]]).cdouble().reshape((1,2,2))
+        self.Observable = self.krons(Z.repeat(n_qubits, 1, 1))
+        self.Entangle = Entangle_layer([[0, 1]])
+        self.Rx1 = Rx_layer([*range(n_qubits)], batch_size, weights = torch.Tensor([np.pi, 0]).reshape(1,2,1,1,1))
+        self.Rx2 = Rx_layer([*range(n_qubits)], batch_size, weights = torch.Tensor([np.pi, np.pi]).reshape(1,2,1,1,1))
         
     def fRot(self, qubits, batch_size):
         return nn.Sequential(
@@ -29,6 +28,72 @@ class BasicModel(nn.Module):
                 Rx_layer(qubits, batch_size)
                 )
         
+    def krons(self, psi):
+        if psi.shape[0] == 2:
+            return torch.kron(psi[0], psi[1])
+        elif psi.shape[0] > 2:
+            return torch.kron(psi[0], self.krons(psi[1:]))
+        elif psi.shape[0] == 1:
+            return psi[0]
+        else:
+            return print("Invalid input")
+
+    def forward(self, x):
+        
+        state = torch.zeros((self.batch_size, self.n_qubits, 1, 2, 1), dtype=torch.cdouble)
+        state[:, :, :, 0, 0] = 1 
+        
+        state = self.Rx1(state)
+        state = self.Entangle(state)
+        state = self.Rx2(state)
+        state = self.Entangle(state)
+        wavefunc = torch.zeros((state.shape[0], 2**state.shape[1], 1)).cdouble()
+        for batch_idx in range(state.shape[0]):
+            for dc_idx in range(state.shape[2]):
+                wavefunc[batch_idx] = wavefunc[batch_idx].add(self.krons(state[batch_idx, :, dc_idx]))   
+                
+        print(wavefunc)
+        
+        O = torch.matmul(wavefunc.transpose(1,2).conj(), torch.matmul(self.Observable, wavefunc))
+        print(O)
+        return state, O
+
+class BasicModel(nn.Module):
+    def __init__(self, n_qubits: int, batch_size: int):
+        super().__init__()   
+        self.n_qubits = n_qubits
+        self.batch_size = batch_size
+        self.fR0 = self.fRot([*range(n_qubits)], batch_size=batch_size)
+        self.fR1 = self.fRot([*range(n_qubits)], batch_size=batch_size) 
+        self.fR2 = self.fRot([*range(n_qubits)], batch_size=batch_size)
+        # self.fR3 = self.fRot([*range(n_qubits)], batch_size=batch_size)
+        # self.fR4 = self.fRot([*range(n_qubits)], batch_size=batch_size)
+        self.fR5 = self.fRot([*range(n_qubits)], batch_size=batch_size)
+        self.fR6 = self.fRot([*range(n_qubits)], batch_size=batch_size)
+        self.fR7 = self.fRot([*range(n_qubits)], batch_size=batch_size)
+        # self.fR8 = self.fRot([*range(n_qubits)], batch_size=batch_size)
+        # self.fR9 = self.fRot([*range(n_qubits)], batch_size=batch_size)
+        Z = torch.Tensor([[1,0],[0,-1]]).cdouble().reshape((1,2,2))
+        self.Observable = self.krons(Z.repeat(n_qubits, 1, 1))
+        #self.Entangle = Entangle_layer([[0, i] for i in range(1,DC)])
+        
+    def fRot(self, qubits, batch_size):
+        return nn.Sequential(
+                Rx_layer(qubits, batch_size),
+                Rz_layer(qubits, batch_size),
+                Rx_layer(qubits, batch_size)
+                )
+    
+    def krons(self, psi):
+        if psi.shape[0] == 2:
+            return torch.kron(psi[0], psi[1])
+        elif psi.shape[0] > 2:
+            return torch.kron(psi[0], self.krons(psi[1:]))
+        elif psi.shape[0] == 1:
+            return psi[0]
+        else:
+            return print("Invalid input")
+        
 
     def forward(self, x):
         #Can probably change this to create one large sequential model
@@ -36,29 +101,58 @@ class BasicModel(nn.Module):
         # ?
         state = torch.zeros((self.batch_size, self.n_qubits, 1, 2, 1), dtype=torch.cdouble)
         state[:, :, :, 0, 0] = 1 
-        #Rx1 = Rx_layer([*range(self.n_qubits)], weights=x, batch_size=self.batch_size)
-        #state = self.fR1(state)
-        #state = Rx1(state)
         
-        state = self.Entangle_1(state)
-        #state = self.Entangle_2(state)
+        DataEncoding0 = Rx_layer([*range(self.n_qubits)], weights=x[:,0], batch_size=self.batch_size)
+        DataEncoding1 = Rx_layer([*range(self.n_qubits)], weights=x[:,1], batch_size=self.batch_size)
+        DataEncoding2 = Rx_layer([*range(self.n_qubits)], weights=x[:,2], batch_size=self.batch_size)
+        # DataEncoding3 = Rx_layer([*range(self.n_qubits)], weights=x[:,3], batch_size=self.batch_size)
+        # DataEncoding4 = Rx_layer([*range(self.n_qubits)], weights=x[:,4], batch_size=self.batch_size)
+        DataEncoding5 = Rx_layer([*range(self.n_qubits)], weights=x[:,5], batch_size=self.batch_size)
+        DataEncoding6 = Rx_layer([*range(self.n_qubits)], weights=x[:,6], batch_size=self.batch_size)
+        DataEncoding7 = Rx_layer([*range(self.n_qubits)], weights=x[:,7], batch_size=self.batch_size)
+        # DataEncoding8 = Rx_layer([*range(self.n_qubits)], weights=x[:,8], batch_size=self.batch_size)
+        # DataEncoding9 = Rx_layer([*range(self.n_qubits)], weights=x[:,9], batch_size=self.batch_size)
         
-        #state = self.fR2(state)
-
-        #state = self.fRx2(state)
-        #state = Rx1(state)
-        #state = self.fRx3(state)
-        #state = Rx1(state)
         
-        #state = Entangle_layer([(3,4), (5,6), (8,6)])(state)
+        state = self.fR0(state)
+        state = DataEncoding0(state)
         
-        #state = self.fRx4(state)
-        #state = Rx1(state)
-        #state = self.fRx5(state)
-        #state = Rx1(state)
-        #state = self.fRz[j](state)
+        state = self.fR1(state)
+        state = DataEncoding1(state)
         
-        O = torch.matmul(state.transpose(3,4).conj(), torch.matmul(self.Observable, state))
-        return state, O.mean(dim=2)
+        state = self.fR2(state)
+        state = DataEncoding2(state)
+        
+        # state = self.fR3(state)
+        # state = DataEncoding3(state)
+        
+        # state = self.fR4(state)
+        # state = DataEncoding4(state)
+        
+        #state = self.Entangle(state)        
+        
+        state = self.fR5(state)
+        state = DataEncoding5(state)
+        
+        state = self.fR6(state)
+        state = DataEncoding6(state)
+        
+        state = self.fR7(state)
+        state = DataEncoding7(state)
+        
+        # state = self.fR8(state)
+        # state = DataEncoding8(state)
+        
+        # state = self.fR9(state)
+        # state = DataEncoding9(state)    
+        
+        wavefunc = torch.zeros((state.shape[0], 2**state.shape[1], 1)).cdouble()
+        for batch_idx in range(state.shape[0]):
+            for dc_idx in range(state.shape[2]):
+                wavefunc[batch_idx] = wavefunc[batch_idx].add(self.krons(state[batch_idx, :, dc_idx]))   
+                
+        
+        O = torch.matmul(wavefunc.transpose(1,2).conj(), torch.matmul(self.Observable, wavefunc))
+        return state, O
     
 

playground.py
@@ -8,28 +8,53 @@ Created on Fri Feb 19 14:09:16 2021
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+from numpy import pi
 
 Z = np.array([[1,0],[0,-1]])
 X = np.array([[0,1],[1,0]])
 I = np.array([[1,0],[0,1]])
 
-plus = np.array([1, -1j])/np.sqrt(2)
+def Rx(theta):
+    a = (theta/2).cos()
+    b = (theta/2).sin()
+    identity = torch.eye(2)
+    off_identity = torch.Tensor([[0,1],[1,0]])
+    return a*identity - 1j*b*off_identity
 
-psi0 = np.zeros(8)
-psi0[0] = 1
+U = torch.Tensor(np.kron(X,X)).cdouble()
+U += 1j*torch.Tensor(np.kron(Z,Z))
+print(U)
+
+for i in range(3):
+    Ur = torch.kron(Rx(torch.rand(1)), Rx(torch.rand(1))).cdouble()
+    t1 = torch.mm(U, Ur)
+    t2 = torch.mm(Ur, U)
+    print(t1-t2)
 
 
-U = np.kron(X,X) + 1j*np.kron(Z,Z)
+def next_batch():
+        
+    def threshold(x):
+        if x > pi:
+            return -1
+        else:
+            return 1
+        
+    X = torch.rand((1,2,1,1,1))*2*pi
+    angles = X.sum(dim=1) % 2*pi
+    Y = angles.apply_(threshold).cdouble()
+    #Y = torch.rand((self.batch_size, self.n_qubits,1,1)).cdouble()*2 -1
+    return X, Y
 
-U1 = np.kron(U,I)
-psi2 =np.matmul(U1,psi0) 
-print(psi2)
-U2 = np.kron(I,U)
-psi3 = np.matmul(U2, psi2)
-print(psi3)
+# Z = torch.Tensor([[1,0],[0,-1]]).cdouble().reshape((1,1,2,2))
+# Observable = Z.repeat((1, 2, 1, 1))
+# x, y = next_batch()
+# print(x, y)
+# state = torch.zeros((1, 2, 2, 1), dtype=torch.cdouble)
+# state[:, :, 0, 0] = 1
+# #print(state.shape, Rx(x.sum(dim=1)).shape)
 
-    
-
-
-
+# state = torch.matmul(Rx(x.sum(dim=1)).cdouble(), state)
+# O = torch.matmul(state.transpose(2,3).conj(), torch.matmul(Observable, state))
+# print(O)
 