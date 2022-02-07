Implemented rank2 decomp CNOT and correct calculation of observable

datafactory.py
@@ -8,16 +8,44 @@ Created on Wed Jan  5 16:27:00 2022
 import torch
 from torch.utils.data import Dataset
 from numpy import pi
+import pickle
+from sklearn.model_selection import train_test_split
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+
+
 
 class DataFactory(Dataset):
-    def __init__(self, batch_size: int, n_blocks: int, n_qubits: int):
-        self.batch_size = batch_size 
-        self.n_blocks = n_blocks
-        self.n_qubits = n_qubits
-            
-    def next_batch(self):        
-        X = torch.rand((self.batch_size,self.n_blocks,1,self.n_qubits,1))*2*pi
-        Y = torch.rand((self.batch_size,self.n_blocks,1,1,1)).cdouble()*2 -1
-        return X, Y
+    def __init__(self, test_train: str = "train"):
+        
+        super().__init__()   
+
+        
+        pickle_off = open("CIFAR-15-"+test_train+".pkl", 'rb')
+        data_dict = pickle.load(pickle_off)
+        self.data = data_dict["data"]
+        self.labels = data_dict["labels"]
+        
+    def __len__(self):
+        return len(self.labels)
     
-    
+    def __getitem__(self, idx):
+        img = self.data[idx]
+        label = self.labels[idx]
+        return img, label
+
+
+batch_size = 1
+train_data = DataLoader(DataFactory(test_train="train"),
+                            batch_size=batch_size, shuffle=True)
+
+x, y = next(iter(train_data))
+if y == 0:
+    lab = "dog"
+elif y == 1:
+    lab = "automobile"
+
+# import matplotlib.pyplot as plt
+# plt.imshow(x.reshape(30,30))
+# plt.colorbar()
+# plt.title(lab)

layers.py
@@ -8,6 +8,8 @@ Created on Wed Dec 22 21:32:13 2021
 import torch
 import torch.nn as nn
 import numpy as np
+from functools import reduce
+import operator
 
 def krons(psi):
     if psi.shape[0] == 2:
@@ -21,7 +23,7 @@ def krons(psi):
 
 class Rx_layer(nn.Module):
     "A layer applying the Rx gate"
-    def __init__(self, batch_size: int, n_blocks: int, n_qubits: int, weights = None):
+    def __init__(self, batch_size: int, n_blocks: int, n_qubits: int, weights = None, weights_spread = 0):
         """
         weights: a tensor of rotation angles, if given from input data
         """
@@ -31,10 +33,11 @@ class Rx_layer(nn.Module):
         self.batch_size = batch_size   
         self.n_blocks = n_blocks
         self.n_qubits = n_qubits
+        self.weights_spread = weights_spread
 
         if weights is None:
-            self.weights = nn.Parameter(torch.Tensor(self.batch_size, self.n_blocks,1,self.n_qubits,1,1))
-            nn.init.uniform_(self.weights, 0, 2*np.pi)
+            self.weights = nn.Parameter(torch.Tensor(1, self.n_blocks,1,self.n_qubits,1,1))
+            nn.init.uniform_(self.weights, -self.weights_spread, self.weights_spread)
             self.weights.cdouble()
         else:
             if weights.shape[0] == batch_size and weights.shape[1] == n_blocks and weights.shape[3] == n_qubits:
@@ -60,19 +63,18 @@ class Rx_layer(nn.Module):
         
         Rxs = self.Rx().cdouble()
         U = torch.zeros(*Rxs.shape[:3], 2**self.n_qubits, 2**self.n_qubits).cdouble()
-        for batch_idx in range(self.batch_size):
+        for batch_idx in range(U.shape[0]):
             for block_idx in range(self.n_blocks):
                 U[batch_idx, block_idx, :] = krons(Rxs[batch_idx, block_idx, 0])
             
         state = torch.matmul(U, state) 
 
         return state
-        
-    ### Consider how to include nograd if weights is given. This might be already incorporated?
-
-class Rz_layer(nn.Module):
-    "A layer applying the Rz gate"
-    def __init__(self, batch_size: int, n_blocks: int, n_qubits: int, weights = None):
+    
+    
+class Ry_layer(nn.Module):
+    "A layer applying the Ry gate"
+    def __init__(self, batch_size: int, n_blocks: int, n_qubits: int, weights = None, weights_spread = 0):
         """
         weights: a tensor of rotation angles, if given from input data
         """
@@ -82,10 +84,63 @@ class Rz_layer(nn.Module):
         self.batch_size = batch_size   
         self.n_blocks = n_blocks
         self.n_qubits = n_qubits
+        self.weights_spread = weights_spread
 
         if weights is None:
-            self.weights = nn.Parameter(torch.Tensor(self.batch_size, self.n_blocks,1,self.n_qubits,1))
-            nn.init.uniform_(self.weights, 0, 2*np.pi)
+            self.weights = nn.Parameter(torch.Tensor(1, self.n_blocks,1,self.n_qubits,1,1))
+            nn.init.uniform_(self.weights, -self.weights_spread, self.weights_spread)
+            self.weights.cdouble()
+        else:
+            if weights.shape[0] == batch_size and weights.shape[1] == n_blocks and weights.shape[3] == n_qubits:
+                self.weights = weights.view(batch_size, n_blocks, 1, n_qubits, 1, 1)     
+            elif weights.shape[3] == 1 and self.n_qubits > 1: 
+                self.weights = weights.repeat(1,1,1,self.n_qubits,1)
+            else:
+                raise RuntimeError("Dimensions of weight tensor are incompatable. Check the input has the right batch size, block and qubit count")
+        
+
+    def Ry(self):
+        a = (self.weights/2).cos()
+        b = (self.weights/2).sin()
+        identity = torch.eye(2)
+        off_identity = torch.Tensor([[0,-1],[1,0]])
+        return a*identity + b*off_identity
+        
+           
+    def forward(self, state):
+        """
+        Take state to be a tensor with dimension batch x blocks x d&c x n-qubit state (2**n x 1) 
+        """
+        
+        Rys = self.Ry().cdouble()
+        U = torch.zeros(*Rys.shape[:3], 2**self.n_qubits, 2**self.n_qubits).cdouble()
+        for batch_idx in range(U.shape[0]):
+            for block_idx in range(self.n_blocks):
+                U[batch_idx, block_idx, :] = krons(Rys[batch_idx, block_idx, 0])
+            
+        state = torch.matmul(U, state) 
+
+        return state
+        
+    ### Consider how to include nograd if weights is given. This might be already incorporated?
+
+class Rz_layer(nn.Module):
+    "A layer applying the Rz gate"
+    def __init__(self, batch_size: int, n_blocks: int, n_qubits: int, weights = None, weights_spread = 0):
+        """
+        weights: a tensor of rotation angles, if given from input data
+        """
+        
+        super().__init__()
+        
+        self.batch_size = batch_size   
+        self.n_blocks = n_blocks
+        self.n_qubits = n_qubits
+        self.weights_spread = weights_spread
+
+        if weights is None:
+            self.weights = nn.Parameter(torch.Tensor(1, self.n_blocks,1,self.n_qubits,1))
+            nn.init.uniform_(self.weights, -self.weights_spread, self.weights_spread)
             self.weights.cdouble()
         else:
             if weights.shape[0] == batch_size and weights.shape[1] == n_blocks and weights.shape[3] == n_qubits:
@@ -107,7 +162,7 @@ class Rz_layer(nn.Module):
         """
         Rzs = self.Rz().cdouble()
         U = torch.zeros(*Rzs.shape[:3], 2**self.n_qubits, 1).cdouble()
-        for batch_idx in range(self.batch_size):
+        for batch_idx in range(U.shape[0]):
             for block_idx in range(self.n_blocks):
                 U[batch_idx, block_idx, :] = krons(Rzs[batch_idx, block_idx, 0]).reshape(-1,1) #Reshape because using flattend matrix
             
@@ -127,10 +182,10 @@ class Hadamard_layer(nn.Module):
     
     
 class CNOT_layer(nn.Module):
-    def __init__(self, block_ids: list, block_size: int, qubit_pairs: list):        
+    def __init__(self, n_blocks: int, block_size: int, qubit_pairs: list):        
         super().__init__()
         
-        self.block_ids = block_ids
+        self.n_blocks = n_blocks
         self.block_size = block_size
         self.pairs = qubit_pairs
         
@@ -174,47 +229,149 @@ class CNOT_layer(nn.Module):
     def forward(self, state):
         for pair in self.pairs:
             CNOT = self.gen_CNOT(pair).cdouble()
-            state[:, self.block_ids] = torch.matmul(CNOT, state[:, self.block_ids])
+            state = torch.matmul(CNOT, state)
         
         return state
                 
     
 class Entangle_layer(nn.Module):
-    """A layer applying the 1/sqrt(2) XX+iZZ gate to all given pairs of qubits"""
-    def __init__(self, qubit_pairs):
+    """A layer applying the 1/sqrt(2) XX+iZZ gate to given pairs of qubits"""
+    def __init__(self, coordinates, n_qubits):
         """
         qubit_pairs: a list of tuples containing pairs of qubits to be entangled in a single layer
         """
         
         super().__init__()
         
-        assert np.any([i!=j for (i,j) in qubit_pairs]), "Invalid pairs included"
-        self.qubit_pairs = qubit_pairs
+        self.n_qubits = n_qubits
+        self.coordinates = coordinates
+        # qubit pairs should be a list of tuples of the form ((block_idx1, qubit_idx1), (block_idx2, qubit_idx2))
+        # the first tuple contains the control qubit coordinate, and the second tuple the target qubit coordinate
         
+        self.I = torch.eye(2).cdouble()
+        self.H = torch.Tensor([[1, 1], [1, -1]])/(2**0.5)
+        self.H = self.H.cdouble()
+        self.Z = torch.Tensor([[1,0],[0,-1]]).cdouble()
+        self.S = torch.Tensor([[1,0],[0,1]]).cdouble()
+        self.S[1,1] = -1j #This is actually S_conj just to save an unneccesary .conj() operation
+
+        
+    def control_U(self, Pauli):
+        return torch.mm(self.S, Pauli)
+    
+    def target_U(self, Pauli):
+        return torch.mm(self.H, torch.mm(self.S, torch.mm(Pauli, self.H)))
+    
+    def embed(self, q_idx: int, control_or_target):
+        U = control_or_target
+        if q_idx > 0 and q_idx < self.n_qubits - 1:
+            pre_I = torch.eye(2**(q_idx))
+            post_I = torch.eye(2**(self.n_qubits-q_idx))
+            return torch.kron(pre_I, torch.kron(U, post_I))
+        elif q_idx == 0:
+            post_I = torch.eye(2**(self.n_qubits-1))
+            return torch.kron(U, post_I)
+        elif q_idx == self.n_qubits - 1:
+            pre_I = torch.eye(2**(q_idx))
+            return torch.kron(pre_I, U)
+        else:
+            raise ValueError("Invalid qubit index given")    
+    
+    def forward(self, state, ent_pairs):
+        """
+        state is a batch_size x n_blocks x d&c x 2**n_qubits x 1
+        ent_pairs is a list of lists containing pairs of idxs of entangled blocks"""
+        # try:
+        #     block_indices = reduce(operator.add, ent_pairs)
+        #     num_repeat_indices = np.unique(block_indices, return_counts=True)[1].max()
+        # except TypeError:
+        #     block_indices = []
+        #     num_repeat_indices = 0
+        state = state.repeat(1,1,2,1,1)
+        for gate in self.coordinates:
+            # ent_pairs.append([gate[0][0], gate[1][0]])
+            
+            for j, (block_idx, qubit_idx) in enumerate(gate):
+                # block_indices.append(block_idx)                
+                # _, count = np.unique(block_indices, return_counts=True)
+                # if count.max() > num_repeat_indices:
+                #     state = state.repeat(1,1,2,1,1)
+                #     num_repeat_indices += 1
+                
+                    
+                reps = state.shape[2] // 2   
+                if j == 0:                 
+                    state[:, block_idx, :reps] = torch.matmul(self.embed(qubit_idx, self.control_U(self.I)), state[:, block_idx, :reps])
+                    state[:, block_idx, reps:] = torch.matmul(self.embed(qubit_idx, self.control_U(self.Z)), state[:, block_idx, reps:])
+                if j == 1:                 
+                    state[:, block_idx, :reps] = torch.matmul(self.embed(qubit_idx, self.target_U(self.I)), state[:, block_idx, :reps])
+                    state[:, block_idx, reps:] = torch.matmul(self.embed(qubit_idx, self.target_U(self.Z)), state[:, block_idx, reps:])
+                       
+        return state, 0#, ent_pairs
+    
+    
+    
+    ### OLD
+class _Entangle_layer(nn.Module):
+    """A layer applying the 1/sqrt(2) XX+iZZ gate to given pairs of qubits"""
+    def __init__(self, coordinates, n_qubits):
+        """
+        qubit_pairs: a list of tuples containing pairs of qubits to be entangled in a single layer
+        """
+        
+        super().__init__()
+        
+        self.n_qubits = n_qubits
+        self.coordinates = coordinates
+        self.ent_blocks = []
+        # qubit pairs should be a list of tuples of the form ((block_idx1, qubit_idx1), (block_idx2, qubit_idx2))
         
         X = torch.Tensor([[0,1],[1,0]]) / 2**0.25
         Z = (1j**0.5)*torch.Tensor([[1,0],[0,-1]]) / 2**0.25
-        self.U = torch.stack((X,Z)).reshape(1,1,2,2,2).cdouble()
         
-    def forward(self, state):
-        indices = []
-        state = state.repeat(1,1,2,1,1)
-        reps = state.shape[2] // 2
-        U = self.U.repeat_interleave(reps, dim=2)
-        num_repeat_indices = 1
-        for i,j in self.qubit_pairs:
-            indices.append(i)
-            indices.append(j)
-            unique, count = np.unique(indices, return_counts=True)
-            if count.max() > num_repeat_indices:
-                state = state.repeat(1,1,2,1,1)
-                U = U.repeat_interleave(2, dim=2)
-                num_repeat_indices += 1
-                    
-            state[:,i] = torch.matmul(U, state[:,i])
-            state[:,j] = torch.matmul(U, state[:,j])
+        if self.n_qubits > 1:       
+            Uxs = torch.zeros((n_qubits, 2**n_qubits, 2**n_qubits))
+            Uzs = torch.zeros((n_qubits, 2**n_qubits, 2**n_qubits))
+            Uxs[0] = torch.kron(X, torch.eye(2**(n_qubits-1)))
+            Uzs[0] = torch.kron(Z, torch.eye(2**(n_qubits-1)))
+            Uxs[-1] = torch.kron(torch.eye(2**(n_qubits-1)), X)
+            Uzs[-1] = torch.kron(torch.eye(2**(n_qubits-1)), Z)
+            for i in range(1, n_qubits-1):
+                Uxs[i] = torch.kron(torch.eye(2**i), torch.kron(X, torch.eye(2**(n_qubits-i-1))))
+                Uxs[i] = torch.kron(torch.eye(2**i), torch.kron(Z, torch.eye(2**(n_qubits-i-1))))
+            self.Uxs = Uxs.cdouble()
+            self.Uzs = Uzs.cdouble()
+        
+        else:
+            self.Uxs = X.view(1,2,2).cdouble()
+            self.Uzs = Z.view(1,2,2).cdouble()
+        
+    def forward(self, state, ent_pairs):
+        """
+        state is a batch_size x n_blocks x d&c x 2**n_qubits x 1
+        ent_pairs is a list of lists containing pairs of idxs of entangled blocks"""
+        try:
+            block_indices = reduce(operator.add, ent_pairs)
+            num_repeat_indices = np.unique(block_indices, return_counts=True)[1].max()
+        except TypeError:
+            block_indices = []
+            num_repeat_indices = 0
+                
+        for gate in self.coordinates:
+            ent_pairs.append([gate[0][0], gate[1][0]])
             
-        return state
+            for block_idx, qubit_idx in gate:
+                block_indices.append(block_idx)                
+                _, count = np.unique(block_indices, return_counts=True)
+                if count.max() > num_repeat_indices:
+                    state = state.repeat(1,1,2,1,1)
+                    num_repeat_indices += 1
+                    
+                reps = state.shape[2] // 2                    
+                state[:, block_idx, :reps] = torch.matmul(self.Uxs[qubit_idx], state[:, block_idx, :reps])
+                state[:, block_idx, reps:] = torch.matmul(self.Uzs[qubit_idx], state[:, block_idx, reps:])            
+                       
+        return state, ent_pairs
   
     
     

main.py
@@ -23,19 +23,28 @@ from datafactory import DataFactory
 from torch.utils.data import DataLoader
 from numpy import pi
 import time
+from torch.utils.data import DataLoader
 
 
-def train(model, optimizer, batch_size, n_blocks, n_qubits):
+
+def train(model, optimizer, batch_size, val_batch_size, n_blocks, n_qubits):
     optim=optimizer
-    criterion = nn.MSELoss()
-
-    dataset = DataFactory(batch_size, n_blocks, n_qubits)
+    criterion = nn.BCELoss()
+    train_data = DataLoader(DataFactory(test_train = "train"),
+                            batch_size=batch_size, shuffle=True)
+    test_data = DataLoader(DataFactory(test_train = "test"),
+                            batch_size=val_batch_size, shuffle=True)
     losses = []
-    for epoch in range(300):
-        x, y = dataset.next_batch() 
+    val_losses = []
+    for epoch in range(301):
+        
+        x, y = next(iter(train_data))
         
         state, output = model(x)
-        loss = criterion(output.real, y.real)
+        output = torch.prod(output.real, dim=1)
+        pred = 0.5*(output.reshape(*y.shape) + 1)
+        y = y.double()
+        loss = criterion(pred, y)
         
         #Backpropagation
         optim.zero_grad()
@@ -43,17 +52,59 @@ def train(model, optimizer, batch_size, n_blocks, n_qubits):
         optim.step()
         
         losses.append(loss.item())
-    return losses
+        
+        if epoch % 10 == 0:
+            val_model = BasicModel(val_batch_size, n_blocks, n_qubits)
+            
+            val_model.load_state_dict(model.state_dict())
+            
+            x_val, y_val = next(iter(test_data))
+            state, output = val_model(x_val)
+            output = torch.prod(output.real, dim=1)
+            pred = 0.5*(output.reshape(*y_val.shape) + 1)
+            y_val = y_val.double()
+            val_loss = criterion(pred, y_val)
+            
+            print(epoch, val_loss.item())
+            val_losses.append(val_loss.item())
+    return losses, val_losses
+
+# lrs = [0.001] #, 0.01, 0.05, 0.1, 0.5, 1]
+
+# for lr in lrs:
+#     print()
+#     print(lr)
+#     start = time.time()
+#     batch_size = 4
+#     n_blocks = 180
+#     n_qubits = 5
+#     model = BasicModel(batch_size, n_blocks, n_qubits)
+#     optim = torch.optim.Adam(model.parameters(), lr=lr)
+#     L_t, L_v = train(model, optim, batch_size, 20, n_blocks, n_qubits)
+    
+#     end = time.time()
+#     plt.plot(L_t)
+#     plt.show()
+#     plt.plot(L_v)
+#     plt.show()
+    
+#     print(end-start)
+
+
+
+
+
+
 
 # times = []
-# QUBITS = [*range(1, 5)]
+# QUBITS = [*range(2, 7)]
 # for q in QUBITS:
 #     start = time.time()
-#     n_qubits = 6
-#     batch_size = q
-#     n_blocks = 2
+#     n_qubits = 5
+#     batch_size = 2 
+#     n_blocks = q
 #     model = BasicModel(batch_size, n_blocks, n_qubits)
-#     optim = torch.optim.AdamW(model.parameters(), lr=0.05)
+#     optim = torch.optim.Adam(model.parameters(), lr=0.05)
 #     L = train(model, optim, batch_size, n_blocks, n_qubits)
 #     end = time.time()
 #     times.append(end-start)
@@ -64,13 +115,15 @@ def train(model, optimizer, batch_size, n_blocks, n_qubits):
 # plt.title("")
 #plt.savefig('QubitsTiming')
     
-n_qubits = 2
+n_qubits = 1
 n_blocks = 2
 batch_size = 1
 weights = torch.ones((batch_size, n_blocks, 1, n_qubits,1,1)).cdouble()*pi
+weights2 = torch.ones((batch_size, n_blocks, 1, n_qubits,1,1)).cdouble()*pi
+weights2[0,0] = 0
 model = TestModel(batch_size, n_blocks, n_qubits)
-s, O = model(weights)
-print(s)
+s, O = model([weights, weights2])
+print(O)
 
 
 # psi, O = model("")

model.py
@@ -5,10 +5,13 @@ Created on Fri Dec 24 14:45:01 2021
 @author: Marius
 """
 
-from layers import Rx_layer, Rz_layer, Hadamard_layer, CNOT_layer, Entangle_layer
+from layers import Rx_layer, Ry_layer, Rz_layer, Hadamard_layer, CNOT_layer, Entangle_layer, krons
 import torch 
 import torch.nn as nn
 import numpy as np
+from functools import reduce
+import operator
+import math
 
 class TestModel(nn.Module):
     def __init__(self, batch_size: int, n_blocks: int, n_qubits: int):
@@ -17,11 +20,12 @@ class TestModel(nn.Module):
         self.n_qubits = n_qubits
         self.batch_size = batch_size
         Z = torch.Tensor([[1,-1]]).cdouble().reshape((1,2,1))
-        self.Observable = self.krons(Z.repeat(n_qubits, 1, 1))
+        self.Observable = krons(Z.repeat(n_qubits, 1, 1))
         #If using not an all Z observable have to change forward() method below too
         self.CNOTs = CNOT_layer([*range(n_blocks)], n_qubits, [(i-1,i) for i in range(1,n_qubits)])
         #self.Rx1 = Rx_layer(batch_size, n_blocks, n_qubits)
         self.Rz2 = Rz_layer(batch_size, n_blocks, n_qubits)
+        self.Entangle = Entangle_layer([((0,0), (1,0))], n_qubits)
         
     def fRot(self):
         return nn.Sequential(
@@ -29,103 +33,126 @@ class TestModel(nn.Module):
                 Rz_layer(self.batch_size, self.n_blocks, self.n_qubits),
                 Rx_layer(self.batch_size, self.n_blocks, self.n_qubits)
                 )
-    
-    def krons(self, psi):
-        if psi.shape[0] > 2:
-            return torch.kron(psi[0], self.krons(psi[1:]))
-        elif psi.shape[0] == 2:
-            return torch.kron(psi[0], psi[1])
-        elif psi.shape[0] == 1:
-            return psi[0]
-        else:
-            return print("Invalid input")
 
     def forward(self, x):
         
         state = torch.zeros((self.batch_size, self.n_blocks, 1, 2**self.n_qubits, 1), dtype=torch.cdouble)
         state[:, :, :, 0, 0] = 1
-        
+        Ry = Ry_layer(self.batch_size, self.n_blocks, self.n_qubits, weights=x[0])
+        Ry2 = Ry_layer(self.batch_size, self.n_blocks, self.n_qubits, weights=x[1])
+
+        state = Ry(state)
         #state = self.Rx1(state)
-        #state = self.Entangle(state)
+        state, ent_pairs = self.Entangle(state, [])
+        state = Ry2(state)
+        state, ent_pairs = self.Entangle(state, [])
         #state = self.Rz2(state)
-        Rx = Rx_layer(self.batch_size, self.n_blocks, self.n_qubits, weights=x)
         #print(Rx)
-        state = Rx(state)
-        print(state)
-        state = self.CNOTs(state)
+        #state = Rx(state)
+        #state, ent_pairs = self.Entangle(state, ent_pairs)
         #print(state)
-        O = torch.matmul(state.transpose(3,4).conj(), self.Observable*state)
+
+        O = torch.zeros(self.batch_size,1).cdouble()
+        a0 = torch.complex(torch.Tensor([1,0]), torch.Tensor([0,-1])) / ((2**0.5)*((-1j)**0.5))
+        alpha = torch.Tensor([1]).cdouble()
+        for j in range(int(math.log2(state.shape[2]))):
+            alpha = torch.kron(alpha, a0)
+        for k in range(state.shape[2]):
+            a_conj = torch.roll(alpha, k).conj()
+            state_conj = torch.roll(state, k, dims=2).transpose(3,4).conj()
+            inn_prods = torch.matmul(state_conj, self.Observable*state)
+            coefs = (a_conj*alpha).view(1,-1)
+            Os = inn_prods.prod(dim=1).view(self.batch_size, -1) * coefs
+            O += Os.sum(dim=1)
+            print()
+        
+        #ent_idxs = reduce(operator.add, ent_pairs)
+        #not_ent = list(set([*range(self.n_blocks)]) - set(ent_idxs))
+        #O[:, not_ent] = torch.matmul(state.transpose(3,4).conj()[:, not_ent], self.Observable*state[:, not_ent])
+        
         return state, O
 
 class BasicModel(nn.Module):
-    def __init__(self, batch_size: int, n_blocks: int, n_qubits: int):
+    def __init__(self, batch_size: int, n_blocks: int, n_qubits: int, weights_spread: float = np.pi):
         super().__init__()   
+        
         self.n_blocks = n_blocks
         self.n_qubits = n_qubits
         self.batch_size = batch_size
+        self.weights_spread = weights_spread
+        
+        def copy_weights(control_seq, target_seq):
+            """Copies weights of one nn.Sequential layer into a target nn.Sequential layer and multiplies by a factor of -1"""
+            for u in range(3):
+                target_seq[u].weights = nn.Parameter(-control_seq[u].weights)
+        
         self.fR0 = self.fRot()
-        self.fR1 = self.fRot() 
+        self.fR1 = self.LAYER(1)
+        copy_weights(self.fR0, self.fR1)
         self.fR2 = self.fRot()
-        # self.fR3 = self.fRot()
-        # self.fR4 = self.fRot()
+        self.fR3 = self.LAYER(2)
+        copy_weights(self.fR0, self.fR1)
+        #self.fR4 = self.LAYER(1)
         self.fR5 = self.fRot()
-        self.fR6 = self.fRot()
-        self.fR7 = self.fRot()
-        self.H = Hadamard_layer(batch_size, n_blocks, n_qubits)
+        #self.H = Hadamard_layer(batch_size, n_blocks, n_qubits)
         Z = torch.Tensor([[1,-1]]).cdouble().reshape((1,2,1))
         #If using not an all Z observable have to change forward() method below too
-        self.Observable = self.krons(Z.repeat(n_qubits, 1, 1))
+        self.Observable = krons(Z.repeat(n_qubits, 1, 1))
         #self.Entangle = Entangle_layer([[0, i] for i in range(1,DC)])
         
+        
+    def LAYER(self, L):
+        return nn.Sequential(
+                Rz_layer(self.batch_size, self.n_blocks, self.n_qubits, weights_spread=self.weights_spread),
+                Rx_layer(self.batch_size, self.n_blocks, self.n_qubits, weights_spread=self.weights_spread),
+                Rz_layer(self.batch_size, self.n_blocks, self.n_qubits, weights_spread=self.weights_spread),
+                CNOT_layer(self.n_blocks, self.n_qubits, [(i, (i+L)%self.n_qubits) for i in range(self.n_qubits)])
+                )    
+    
     def fRot(self):
         return nn.Sequential(
-                Rz_layer(self.batch_size, self.n_blocks, self.n_qubits),
-                Rx_layer(self.batch_size, self.n_blocks, self.n_qubits),
-                Rz_layer(self.batch_size, self.n_blocks, self.n_qubits)
-                )
-
-    def krons(self, psi):
-        if psi.shape[0] > 2:
-            return torch.kron(psi[0], self.krons(psi[1:]))
-        elif psi.shape[0] == 2:
-            return torch.kron(psi[0], psi[1])
-        elif psi.shape[0] == 1:
-            return psi[0]
-        else:
-            return print("Invalid input")
-    
+                Rz_layer(self.batch_size, self.n_blocks, self.n_qubits, weights_spread=self.weights_spread),
+                Rx_layer(self.batch_size, self.n_blocks, self.n_qubits, weights_spread=self.weights_spread),
+                Rz_layer(self.batch_size, self.n_blocks, self.n_qubits, weights_spread=self.weights_spread),
+                )  
 
     def forward(self, x):
         #Can probably change this to create one large sequential model
         # then a single forward call at the end
         # ?
+                
+        X = x.reshape((self.batch_size, self.n_blocks, 1, self.n_qubits, 1))
+        
+        Ry_data = Ry_layer(self.batch_size, self.n_blocks, self.n_qubits, weights=X)
+
+        
         state = torch.zeros((self.batch_size, self.n_blocks, 1, 2**self.n_qubits, 1), dtype=torch.cdouble)
-        state[:, :, :, -1, 0] = 1 
+        state[:, :, :, :, 0] = 2**(-self.n_qubits/2)
         
-        state = self.H(state)
-        # state = self.fR0(state)
+        #state = self.H(state) #Implicitly included in state
         
-        # state = self.fR1(state)
+        state = Ry_data(state)
+
+        state = self.fR0(state)
         
-        # state = self.fR2(state)
+        state = Ry_data(state)
         
-        # state = self.fR3(state)
+        state = self.fR1(state)
         
-        # state = self.fR4(state)
+        state = Ry_data(state)
         
-        #state = self.Entangle(state)        
+        state = self.fR2(state)
         
-        # state = self.fR5(state)
+        state = Ry_data(state)
+
+        state = self.fR3(state)
         
-        # state = self.fR6(state)
+        #state = Ry_data(state)
         
-        # state = self.fR7(state)
-        
-        # state = self.fR8(state)
-        
-        # state = self.fR9(state)
-     
-        
+        #state = self.fR4(state)
+                
+        state = self.fR5(state)
+                
         O = torch.matmul(state.transpose(3,4).conj(), self.Observable*state)
         return state, O
     

playground.py
@@ -9,67 +9,190 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 from numpy import pi
+import pandas as pd
 
 Z = np.array([[1,0],[0,-1]])
-X = np.array([[0,1],[1,0]])
+#X = np.array([[0,1],[1,0]])
 I = torch.eye(2)
 
-def CNOT(pair, n_qubits):
-    if pair[0] > pair[1]:
-        U = torch.zeros((2**n_qubits, 2**n_qubits))
-        I = torch.eye(2**(n_qubits-1)).flatten()
-        stride = 2**(n_qubits-pair[0]-1)
-        unstride = 2**(pair[0])
-        mask = torch.Tensor([[1,0],[0,0]]).bool()
-        mask = mask.repeat_interleave(stride, dim=0).repeat_interleave(stride, dim=1)
-        mask = mask.repeat(unstride, unstride)
-        U[mask] = I
-        
-        I = torch.eye(2**(n_qubits-2)).flatten()
-        outer_mask_1 = torch.Tensor([[0,0], [1,0]]).bool()
-        outer_mask_2 = torch.Tensor([[0,1], [0,0]]).bool()
-        inner_mask = torch.Tensor([[0,0],[0,1]]).bool()
-        inner_mask = inner_mask.repeat_interleave(stride, dim=0).repeat_interleave(stride, dim=1)
-        inner_mask = inner_mask.repeat(unstride//2, unstride//2)
-        mask = torch.kron(outer_mask_1, inner_mask)
-        U[mask] = I
-        mask = torch.kron(outer_mask_2, inner_mask)
-        U[mask] = I
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
 
-    elif pair[0] < pair[1]:
-        X = torch.Tensor([[0,1],[1,0]])
-        I1 = torch.eye(2**(pair[1] - pair[0] - 1))
-        I2 = torch.eye(2**(n_qubits - pair[1] - 1))
-        M = torch.kron(I1, torch.kron(X, I2))
-        U = torch.eye(M.shape[0]*2)
-        U[U.shape[0]//2:, U.shape[0]//2:] = M
-        pre_I = torch.eye(2**pair[0])
-        U = torch.kron(pre_I, U)
-        
-    else:
-        raise RuntimeError("Invalid qubit pair given")
-        
-    return U
-        
-        
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler((-pi/2, pi/2))
 
-def apply(U, state):
-    print(torch.matmul(U, state))
+import pickle
+
+# data = pd.read_csv("../slice_localization_data/slice_localization_data.csv", nrows=1161)
+# X = data[data.keys()[1:-1]]
+# Y = data["reference"]
+# scaler.fit(X)
+# datat = scaler.transform(X)
+
+
+# mnist = fetch_openml('mnist_784')
+# # test_size: what proportion of original data is used for test set
+# train_img, test_img, train_lbl, test_lbl = train_test_split(
+#     mnist.data, mnist.target, test_size=1/7.0, random_state=0)
+
+# def unpickle(file):
+#     import pickle
+#     with open(file, 'rb') as fo:
+#         dict = pickle.load(fo, encoding='bytes')
+#     return dict
+
+# batches = [*range(1,6)]
+# for idx in batches:
+#     CIFAR = unpickle("../cifar-10-batches-py/data_batch_"+str(idx))
+#     labels_ = np.array(CIFAR[b"labels"])
+#     idxs_0 = np.argwhere(labels_ == 1)
+#     idxs_1 = np.argwhere(labels_ == 5)
+#     try:
+#         X = np.concatenate((X, CIFAR[b"data"][idxs_0]))
+#         X = np.concatenate((X, CIFAR[b"data"][idxs_1]))
+#         Y = np.concatenate((Y, labels_[idxs_0]))
+#         Y = np.concatenate((Y, labels_[idxs_1]))
+#     except NameError:
+#         X = CIFAR[b"data"][idxs_0]
+#         X = np.concatenate((X, CIFAR[b"data"][idxs_1]))
+#         Y = labels_[idxs_0]
+#         Y = np.concatenate((Y, labels_[idxs_1]))
+        
+# X = X[:,0]
+# grey = (X[:,:1024] + X[:, 1024:2048] + X[:, 2048:] ) / 3
+
+# grey = grey.reshape((-1,32,32))[:,1:-1, 1:-1].reshape((-1,900))
+# print(grey.shape)
+# scaler.fit(grey)
+# data = scaler.transform(grey)
+# print(data.shape)
+
+# def relabel(Y):
+#     Y[Y==5] = 0
+#     #Y[Y==1] = 1
+#     return Y
+
+# Y = relabel(Y)
+
+
+
+
+# cifar15 = {"data": data, "labels": Y}
+
+
+# pickling_on = open("CIFAR-15-train.pkl","wb")
+# pickle.dump(cifar15, pickling_on)
+# pickling_on.close()
+
+# X, Y = None, None
+
+
+# CIFAR = unpickle("../cifar-10-batches-py/test_batch")
+# labels_ = np.array(CIFAR[b"labels"])
+# idxs_0 = np.argwhere(labels_ == 1)
+# idxs_1 = np.argwhere(labels_ == 5)
+# try:
+#     X = np.concatenate((X, CIFAR[b"data"][idxs_0]))
+#     X = np.concatenate((X, CIFAR[b"data"][idxs_1]))
+#     Y = np.concatenate((Y, labels_[idxs_0]))
+#     Y = np.concatenate((Y, labels_[idxs_1]))
+# except ValueError:
+#     X = CIFAR[b"data"][idxs_0]
+#     X = np.concatenate((X, CIFAR[b"data"][idxs_1]))
+#     Y = labels_[idxs_0]
+#     Y = np.concatenate((Y, labels_[idxs_1]))
+        
+# X = X[:,0]
+# grey = (X[:,:1024] + X[:, 1024:2048] + X[:, 2048:] ) / 3
+
+# grey = grey.reshape((-1,32,32))[:,1:-1, 1:-1].reshape((-1,900))
+# print(grey.shape)
+# scaler.fit(grey)
+# data = scaler.transform(grey)
+# print(data.shape)
+
+# def relabel(Y):
+#     Y[Y==5] = 0
+#     #Y[Y==1] = 1
+#     return Y
+
+# Y = relabel(Y)
+
+
+
+
+# cifar15 = {"data": data, "labels": Y}
+
+
+# pickling_on = open("CIFAR-15-test.pkl","wb")
+# pickle.dump(cifar15, pickling_on)
+# pickling_on.close()
+
+# from sklearn.decomposition import PCA
+
+# scaler.fit(grey)
+# grey = scaler.transform(grey)
+
+# cifar_comps = []
+
+# for r in [0.5, 0.75, 0.9, 0.95, 0.99, 0.999]:
+#     # Make an instance of the Model
+#     pca = PCA(r)
+
+#     pca.fit(grey)
+
+#     print(r, pca.n_components_)
+#     cifar_comps.append(pca.n_components_)
     
-qubits = 4
-state = torch.zeros((2**qubits, 1))
-state[8] = 1
-print(state)
+# mnist_comps = []
+    
+# scaler.fit(train_img)
 
-U1 = CNOT((0,1), qubits)
-U2 = CNOT((1,2), qubits)
-print(torch.matmul(U1, U1))
-print()
-# apply(U1,state)
-# apply(torch.matmul(U2, U1),state)
+# # Apply transform to both the training set and the test set.
+# train_img = scaler.transform(train_img)
+# test_img = scaler.transform(test_img)
 
+# for r in [0.5, 0.75, 0.9, 0.95, 0.99, 0.999]:
+#     # Make an instance of the Model
+#     pca = PCA(r)
 
+#     pca.fit(train_img)
 
+#     print(r, pca.n_components_)
+#     mnist_comps.append(pca.n_components_)
+    
+    
+# print(pd.DataFrame({"Required explained variance": [0.5, 0.75, 0.9, 0.95, 0.99, 0.999],
+#                     "# Principle components CIFAR15": cifar_comps,
+#                     "# Principle components MNIST": mnist_comps}).to_string())
+
+I = np.identity(2, dtype=complex)
+Z = I.copy()
+Z[1,1] = -1
+S = I.copy()
+S[1,1] = -1j
+H = np.array([[1,1],[1,-1]]) / np.sqrt(2)
+
+IH = np.kron(I,H)
+SSc = np.kron(S, S)
+U = np.kron(I,I) - 1j*np.kron(Z,Z)
+U /= np.sqrt(2)*(1-1j)
+
+def ap(U1, U2):
+    return np.matmul(U1, U2)
+
+print(ap(IH, ap(SSc, ap(U, IH))).round(2))
+
+print(ap(H, ap(S,H)))
+
+Uc1 = S
+Uc2 = ap(S, Z)
+
+Ut1 = ap(H, ap(S, H))
+Ut2 = ap(H, ap(S, ap(Z, H)))
+
+T = (np.kron(Uc1, Ut1) - 1j*np.kron(Uc2, Ut2))/ ((2**0.5)*((-1j)**0.5))
+print(ap(T, ap(T,T)))
 
 
 